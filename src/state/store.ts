63542a84b9d70bd0c51d8b76e88213c6acabc56a
Jesus Santander <jsantanders@outlook.com>
2021-12-21 17:51:36 -0400

    adds redux persist (#8)

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,28 @@
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { basketReducer } from "dataflows/Basket/BasketSlice";
 import { categoryReducer } from "dataflows/Category/CategorySlice";
 import { checkoutReducer } from "dataflows/Checkout/CheckoutSlice";
+import { productReducer } from "dataflows/Product/IProductSlice";
 
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-import { productReducer } from "../dataflows/Product/IProductSlice";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["basket"],
+};
 
 const rootReducer = combineReducers({
   basket: basketReducer,
@@ -12,10 +30,19 @@ const rootReducer = combineReducers({
   product: productReducer,
   checkout: checkoutReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
