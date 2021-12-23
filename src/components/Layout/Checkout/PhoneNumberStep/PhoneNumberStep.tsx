a3758b44b73652fc337bb521f16eed2895db2d77
Francisco Valloire <frajova@MacBook-Pro-de-mbpro.local>
2021-12-23 15:48:42 -0300

    ADD: library

--- src/components/Layout/Checkout/PhoneNumberStep/PhoneNumberStep.tsx
+++ src/components/Layout/Checkout/PhoneNumberStep/PhoneNumberStep.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import PhoneInput from "react-phone-input-2";
 
 import { FormControl, FormErrorMessage, FormLabel, Input, Text, VStack } from "@chakra-ui/react";
 
@@ -11,6 +12,7 @@ import { IPhoneNumberStepProps } from "./IPhoneNumberStepProps";
  **/
 export const PhoneNumberStep = (props: IPhoneNumberStepProps): ReactElement => {
   const { errors, register } = props;
+  console.log("props", props);
   return (
     <VStack spacing={55}>
       <Text pt={10} fontSize="2xl" fontWeight="600">
@@ -20,6 +22,11 @@ export const PhoneNumberStep = (props: IPhoneNumberStepProps): ReactElement => {
         <FormLabel>Número de celular</FormLabel>
         <Input size="md" errorBorderColor="red.300" {...register("phone")} />
         <FormErrorMessage>{errors.phone?.message}</FormErrorMessage>
+        {/* <PhoneInput
+          country={"mx"}
+          value={this.state.phone}
+          onChange={phone => this.setState({ phone })}
+        /> */}
       </FormControl>
     </VStack>
   );
