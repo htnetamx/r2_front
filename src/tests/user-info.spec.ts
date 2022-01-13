c200a785744cb36ae7a4c34e5dcbf4e0088353ec
ivansmorales <ivann_mg@hotmail.com>
2022-01-13 11:40:07 -0600

    Add TODO comment

--- src/tests/user-info.spec.ts
+++ src/tests/user-info.spec.ts
@@ -12,20 +12,27 @@ test.beforeAll(async ({ playwright }) => {
   });
 });
 
+// TODO: For testing purposes.
 test("Should get product by id", async ({ page }) => {
   const product = await apiContext.get("/product/12");
   expect(product.ok());
   await page.goto("/info");
 });
 
+test("Should get current profit", async ({ page }) => {
+  // const currentProfit = await apiContext.get("/currentProfit");
+  // expect(currentProfit).toBeGreaterThanOrEqual(0);
+  await page.goto("/info");
+});
+
 test("Should submit info form", async ({ page }) => {
   await page.goto("/info");
   // Expect an element "to be visible".
-  await page.click('p:has-text("Tu ganancia en esta orden")');
-  await expect(page.locator('p:has-text("Mis datos")')).toBeVisible();
+  await page.click("p:has-text(\"Tu ganancia en esta orden\")");
+  await expect(page.locator("p:has-text(\"Mis datos\")")).toBeVisible();
   await page.focus("input[id=\"name\"]");
-  await page.fill('input[id="name"]', "Iván Morales");
-  await page.fill('input[id="address"]', "Calle H. Mz. 141 Lt. 8");
-  await page.click('button, input[type="button"], input[type="submit"] >> text="Guardar cambios"');
+  await page.fill("input[id=\"name\"]", "Iván Morales");
+  await page.fill("input[id=\"address\"]", "Calle H. Mz. 141 Lt. 8");
+  await page.click("button, input[type=\"button\"], input[type=\"submit\"] >> text=\"Guardar cambios\"");
   await page.pause();
 });
